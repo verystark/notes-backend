9280ffe5e2edbf3ade8e56057cf328302e6a046a
verystark <tuomaspaloheimo05@gmail.com>
2026-07-08 16:17:28 +0300

    mongodb atlas type shi

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://tuomaspaloheimo05_db_user:3NLSzcSzaulkPP4F@cluster0.qbt4kuu.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+    content: 'now i understand it',
+    important: true,
+})
+
+//note.save().then(result => {
+  //  console.log('note saved')
+    //mongoose.connection.close()
+//})
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
