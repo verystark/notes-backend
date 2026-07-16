6ed773982e3bd8998d9b81b08dc40e1875e1d10d
verystark <tuomaspaloheimo05@gmail.com>
2026-07-16 19:16:25 +0300

    eslint workflow updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import globals from 'globals'
 import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin'
 
 export default [
   js.configs.recommended,
@@ -10,5 +11,17 @@ export default [
       globals: { ...globals.node },
       ecmaVersion: 'latest',
     },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    }
   },
+  {
+    ignores: ['dist/**'],
+  }
 ]
