a5f0d7768b06a97b90d1061e03960b73f72fa76e
verystark <tuomaspaloheimo05@gmail.com>
2026-07-16 19:28:07 +0300

    add lint configurations and apply fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,11 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     }
   },
   {
