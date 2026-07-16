d102e85194fe75d2c13b12348ebcc2fe946b3f6b
verystark <tuomaspaloheimo05@gmail.com>
2026-07-16 19:08:21 +0300

    eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from 'globals'
+import js from '@eslint/js'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+  },
+]
