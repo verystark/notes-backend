f6180c10a225076541f454a794b41590d61aec98
verystark <tuomaspaloheimo05@gmail.com>
2026-07-17 13:53:06 +0300

    modularize express app and connecting files

--- models/note.js
+++ models/note.js
@@ -1,18 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-mongoose.set('strictQuery', false)
-
-const url = config.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
