c3a82c28c9107b2a8a39e16158897b8b87e08797
verystark <tuomaspaloheimo05@gmail.com>
2026-07-22 18:51:58 +0300

    add api test for post request with empty object

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -67,6 +67,21 @@ test('a valid note can be added ', async () => {
   assert(contents.includes('async/await simplifies making async calls'))
 })
 
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const response = await api.get('/api/notes')
+
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
