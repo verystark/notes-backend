cb28b52159dbff5b1d1f670d62cbe1f63f822fcf
verystark <tuomaspaloheimo05@gmail.com>
2026-07-22 17:52:37 +0300

    add api tests

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = 'mongodb+srv://tuomaspaloheimo05_db_user:3NLSzcSzaulkPP4F@cluster0.qbt4kuu.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0'
+const url = `mongodb+srv://tuomaspaloheimo05_db_user:${password}@cluster0.qbt4kuu.mongodb.net/testNoteApp?appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url, { family: 4 })
@@ -20,18 +20,11 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'now i understand it',
+  content: 'Jonathan Toes',
   important: true,
 })
 
-//note.save().then(result => {
-//  console.log('note saved')
-//mongoose.connection.close()
-//})
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
+note.save().then(() => {
+  console.log('note saved')
   mongoose.connection.close()
 })
