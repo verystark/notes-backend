c20aa2727ed11f0ef12c1edbf372aae81f809744
verystark <tuomaspaloheimo05@gmail.com>
2026-07-22 18:49:27 +0300

    add api test for post

--- controllers/notes.js
+++ controllers/notes.js
@@ -28,7 +28,7 @@ notesRouter.post('/', (request, response, next) => {
 
   note.save()
     .then(savedNote => {
-      response.json(savedNote)
+      response.status(201).json(savedNote)
     })
     .catch(error => next(error))
 })
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -46,6 +46,27 @@ test('a specific note is within the returned notes', async () => {
   assert(contents.includes('HTML is easy'))
 })
 
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(r => r.content)
+
+  assert.strictEqual(response.body.length, initialNotes.length + 1)
+
+  assert(contents.includes('async/await simplifies making async calls'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
