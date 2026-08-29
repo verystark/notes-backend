17a4de77844d169374e6651f9b032ad08478ccb9
verystark <tuomaspaloheimo05@gmail.com>
2026-08-29 21:07:00 +0300

    add e2e test for logging in

--- e2e-test/tests/note_app.spec.js
+++ e2e-test/tests/note_app.spec.js
@@ -8,4 +8,14 @@ describe('Note app', () => {
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2026')).toBeVisible()
   })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByRole('textbox').first().fill('mluukkai')
+    await page.getByRole('textbox').last().fill('salainen')
+    await page.getByRole('button', { name: 'login' }).click()
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
 })
