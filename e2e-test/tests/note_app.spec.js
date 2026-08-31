c93c4be971d7e232d9e7ba1a73255dd7364892d4
verystark <tuomaspaloheimo05@gmail.com>
2026-08-31 23:12:02 +0300

    add e2e test for creating new note

--- e2e-test/tests/note_app.spec.js
+++ e2e-test/tests/note_app.spec.js
@@ -21,4 +21,20 @@ describe('Note app', () => {
     await page.getByRole('button', { name: 'login' }).click()
     await expect(page.getByText('logged in as Matti Luukkainen')).toBeVisible()
   })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new note can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new note' }).click()
+      await page.getByRole('textbox').fill('new note created by playwright')
+      await page.getByRole('button', { name: 'save' }).click()
+      await expect(page.getByText('new note created by playwright')).toBeVisible()
+    })
+  })
 })
