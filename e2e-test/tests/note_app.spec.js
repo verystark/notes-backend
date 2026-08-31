56805674584bf595147ddefbefe8345de8ea0720
verystark <tuomaspaloheimo05@gmail.com>
2026-08-31 23:01:25 +0300

    add beforeEach before all tests

--- e2e-test/tests/note_app.spec.js
+++ e2e-test/tests/note_app.spec.js
@@ -1,6 +1,9 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
   test('front page can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173')
 
