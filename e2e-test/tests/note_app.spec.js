fa75ebb128eb8f6d33e134fca5cfe151e2f03b19
verystark <tuomaspaloheimo05@gmail.com>
2026-09-21 22:52:57 +0300

    add e2e test for login in with wrong password

--- e2e-test/tests/note_app.spec.js
+++ e2e-test/tests/note_app.spec.js
@@ -30,6 +30,18 @@ describe('Note app', () => {
     await expect(page.getByText('logged in as Matti Luukkainen')).toBeVisible()
   })
 
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByLabel('username').fill('mluukkai')
+    await page.getByLabel('password').fill('wrong')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    const errorDiv = page.locator('.error')
+    await expect(errorDiv).toContainText('wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+  })
+
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
       await page.getByRole('button', { name: 'login' }).click()
